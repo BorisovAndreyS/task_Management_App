38d5eea4c89fcd65e56db881355aabc626f91db2
helmut <helmut.andrey@gmail.com>
2025-03-16 11:42:41 +0500

    add TaskList.js

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,44 @@
-from app import app
+from app import app, db
+from app.models import Task, User
+from flask import request, jsonify
 
-@app.route('/')
-def index():
-    return "Hello, Task Management App!"
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    return jsonify([task.to_dict() for task in tasks])
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    new_task = Task(
+        title=data['title'],
+        description=data.get('description'),
+        owner_id=data['owner_id'],
+        status=data.get('status', "невыполнена")
+    )
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify(new_task.to_dict()), 201
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    task = Task.query.get_or_404(id)
+    return jsonify(task.to_dict())
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    data = request.get_json()
+    task = Task.query.get_or_404(id)
+    task.title = data['title']
+    task.description = data.get('description')
+    task.status = data.get('status', task.status)
+    task.owner_id = data['owner_id']
+    db.session.commit()
+    return jsonify(task.to_dict())
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return '', 204
